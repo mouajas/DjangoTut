67b24a350a24de54940940faa281bf4b272ed4bb
Jasmine Moua <jmoua@csumb.edu>
2021-04-17 20:41:02 +0000

    Added another header for home_view

--- pages/views.py
+++ pages/views.py
@@ -3,4 +3,4 @@ from django.shortcuts import render
 
 # Create your views here.
 def home_view(*args, **kwargs): # *args, **kwargs
-    return HttpResponse("<h1>Hello World</h1>") # string of html code
+    return HttpResponse("<h1>Hello World</h1><h2>Subheader</h2>") # string of html code
